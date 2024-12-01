f1d1763b98ecaca8c38ff4921b37d4a5e5743d82
Ratul Hasan <66786636+Onnesok@users.noreply.github.com>
2024-12-02 01:44:55 +0600

    textfield

---
+++ lib/widgets/custom_input_field.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class CustomInputField extends StatelessWidget {
+  final TextEditingController controller;
+  final String hintText;
+  final IconData icon;
+  final bool isPassword;
+  final bool isPasswordVisible;
+  final VoidCallback? onTogglePasswordVisibility;
+  final String? Function(String?)? validator;
+
+  const CustomInputField({
+    Key? key,
+    required this.controller,
+    required this.hintText,
+    required this.icon,
+    this.isPassword = false,
+    this.isPasswordVisible = false,
+    this.onTogglePasswordVisibility,
+    this.validator,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: TextFormField(
+        style: TextStyle(color: Colors.black),
+        controller: controller,
+        obscureText: isPassword && !isPasswordVisible,
+        decoration: InputDecoration(
+          hintText: hintText,
+          prefixIcon: Icon(icon),
+          suffixIcon: isPassword
+              ? IconButton(
+            icon: Icon(
+              isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+            ),
+            onPressed: onTogglePasswordVisibility,
+          )
+              : null,
+        ),
+        validator: validator,
+      ),
+    );
+  }
+}
