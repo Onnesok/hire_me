1c7c340a7c4d8ae8f910f500b885398ea6ff8f8e
Ratul Hasan <66786636+Onnesok@users.noreply.github.com>
2024-12-03 21:21:56 +0600

    solved main login

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:hire_me/controller/login_controller.dart';
 import 'package:hire_me/home.dart';
 import 'package:hire_me/service/profile_provider.dart';
 import 'package:hire_me/theme/app_theme.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Ensure the app only runs in portrait mode
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+  // Load SharedPreferences before running the app
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  bool isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
+
+  runApp(MyApp(isLoggedIn: isLoggedIn));
 }
 
 class MyApp extends StatelessWidget {
+  final bool isLoggedIn;
+  const MyApp({super.key, required this.isLoggedIn});
+
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
@@ -21,21 +36,11 @@ class MyApp extends StatelessWidget {
         theme: AppTheme.lightTheme,
         darkTheme: AppTheme.darkTheme,
         themeMode: ThemeMode.system, // switch between light/dark based on system .....
-        home: AuthCheck(),
+        home: isLoggedIn
+            ? Home() // Navigate to home if logged in
+            : LoginController(),
       ),
     );
-  }
-}
-
-class AuthCheck extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final profileProvider = Provider.of<ProfileProvider>(context);
-
-    // Check login status
-    return profileProvider.isLoggedIn
-        ? Home() // Navigate to home if logged in
-        : LoginController(); // Navigate to login screen if not logged in
   }
 }
 
