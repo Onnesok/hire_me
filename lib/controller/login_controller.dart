8e14635c08f18b62067ea97f53ab700b598ca4bb
Ratul Hasan <66786636+Onnesok@users.noreply.github.com>
2024-12-03 21:22:33 +0600

    api nai

--- lib/controller/login_controller.dart
+++ lib/controller/login_controller.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:hire_me/home.dart';
 import 'package:hire_me/view/registration_view.dart';
 import 'package:provider/provider.dart';
 import '../service/login_api_service.dart';
@@ -17,30 +19,33 @@ class _LoginControllerState extends State<LoginController> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
 
-  void _handleLogin() async {
+  void _handleLogin() async {   // TODO: not handled yet
     if (_formKey.currentState!.validate()) {
-      try {
-        final tokens = await AuthService.login(
-          _emailController.text,
-          _passwordController.text,
-        );
-        final user = await AuthService.fetchUserProfile(tokens["accessToken"]!);
-
-        // Store user profile and tokens
-        final profileProvider = Provider.of<ProfileProvider>(context, listen: false);
-        await profileProvider.storeAccessToken(tokens["accessToken"]!);
-        await profileProvider.saveProfileData({
-          "name": user.name,
-          "email": user.email,
-        });
-
-        //Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> Homeview()));
-      } catch (e) {
-        print(e);
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text("Login failed")),
-        );
-      }
+      final profileProvider = Provider.of<ProfileProvider>(context, listen: false);
+      await profileProvider.updateLoginStatus(true);
+      Fluttertoast.showToast(msg: "saved: ${profileProvider.isLoggedIn}");
+      // try {
+      //   final tokens = await AuthService.login(
+      //     _emailController.text,
+      //     _passwordController.text,
+      //   );
+      //   final user = await AuthService.fetchUserProfile(tokens["accessToken"]!);
+      //
+      //   // Store user profile and tokens
+      //  final profileProvider = Provider.of<ProfileProvider>(context, listen: false);
+      //   await profileProvider.storeAccessToken(tokens["accessToken"]!);
+      //   await profileProvider.saveProfileData({
+      //     "name": user.name,
+      //     "email": user.email,
+      //   });
+      //
+      //   Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> Home()));
+      // } catch (e) {
+      //   print(e);
+      //   ScaffoldMessenger.of(context).showSnackBar(
+      //     SnackBar(content: Text("Login failed")),
+      //   );
+      // }
     }
   }
 
