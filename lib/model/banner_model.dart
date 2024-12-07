a0f199af3839b2ef44db2a162d3d7e806c0fe7ec
Ratul Hasan <66786636+Onnesok@users.noreply.github.com>
2024-12-08 01:40:32 +0600

    banner model

---
+++ lib/model/banner_model.dart
@@ -0,0 +1,40 @@
+class Banner {
+  Banner({
+    this.title = '',
+    this.imagePath = '',
+  });
+
+  String title;
+  String imagePath;
+
+  static List<Banner> BannerList = <Banner>[
+    Banner(
+      imagePath: 'assets/banner/b2.png',
+      title: 'banner1',
+    ),
+    Banner(
+      imagePath: 'assets/banner/b3.png',
+      title: 'banner2',
+    ),
+    Banner(
+      imagePath: 'assets/banner/b4.png',
+      title: 'banner3',
+    ),
+    Banner(
+      imagePath: 'assets/banner/b5.png',
+      title: 'banner4',
+    ),
+    Banner(
+      imagePath: 'assets/banner/b1.png',
+      title: 'banner5',
+    ),
+    Banner(
+      imagePath: 'assets/banner/b6.png',
+      title: 'banner6',
+    ),
+    Banner(
+      imagePath: 'assets/banner/b7.png',
+      title: 'banner7',
+    ),
+  ];
+}
