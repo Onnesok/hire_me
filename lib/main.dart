b84a8e7f2f4aa58ad5d08cd1d9b629c46d133559
Ratul Hasan <66786636+Onnesok@users.noreply.github.com>
2024-12-10 01:02:37 +0600

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,73 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:fluttertoast/fluttertoast.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hire_me/bottom_appbar.dart';
 import 'package:hire_me/controller/login_controller.dart';
-import 'package:hire_me/home.dart';
 import 'package:hire_me/service/profile_provider.dart';
+import 'package:hire_me/service/themeprovider.dart';
 import 'package:hire_me/theme/app_theme.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:hire_me/service/local_notification.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Ensure the app only runs in portrait mode
-  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  // Load .env file
+  await dotenv.load(fileName: ".env");
 
-  // Load SharedPreferences before running the app
+  // Lock orientation to portrait mode
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  // Initialize local notifications
+  await LocalNotificationService.initialize();
+
+  // Initialize SharedPreferences for theme persistence
   SharedPreferences prefs = await SharedPreferences.getInstance();
   bool isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
+
+  // Initialize OneSignal using the App ID from .env
+  String? oneSignalAppId = dotenv.env['ONESIGNAL_APP_ID'];
+  if (oneSignalAppId != null) {
+    OneSignal.Debug.setLogLevel(OSLogLevel.verbose);
+    OneSignal.initialize(oneSignalAppId);
+    OneSignal.Notifications.requestPermission(true);
+  } else {
+    throw Exception('ONESIGNAL_APP_ID not found in .env');
+  }
 
   runApp(MyApp(isLoggedIn: isLoggedIn));
 }
 
 class MyApp extends StatelessWidget {
   final bool isLoggedIn;
-  const MyApp({super.key, required this.isLoggedIn});
+
+  const MyApp({Key? key, required this.isLoggedIn}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => ProfileProvider()),
+        ChangeNotifierProvider(create: (_) => ThemeProvider()),
       ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: AppTheme.lightTheme,
-        darkTheme: AppTheme.darkTheme,
-        themeMode: ThemeMode.system, // switch between light/dark based on system .....
-        home: isLoggedIn
-            ? CustomBottomAppBar() // Navigate to home if logged in
-            : LoginController(),
+      child: Consumer<ThemeProvider>(
+        builder: (context, themeProvider, _) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: AppTheme.lightTheme,
+            darkTheme: AppTheme.darkTheme,
+            themeMode: themeProvider.themeMode,
+            home: isLoggedIn
+                ? const CustomBottomAppBar() // Navigating to home if logged in
+                : const LoginController(),
+          );
+        },
       ),
     );
   }
 }
-
